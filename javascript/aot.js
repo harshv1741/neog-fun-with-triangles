5355c39bdd2d9668cfc4bf7165dc36e4b84f2e05
harshv1741 <ardostree2001@outlook.com>
2022-10-02 11:40:08 +0530

    Area of Triangle: finished

--- javascript/aot.js
+++ javascript/aot.js
@@ -1,36 +1,31 @@
-const s1 = document.querySelector("#ang1");
-const s2 = document.querySelector("#ang2");
-const s3 = document.querySelector("#ang3");
+const s1 = document.querySelector("#s1");
+const s2 = document.querySelector("#s2");
+const s3 = document.querySelector("#s3");
 const aot_submit = document.querySelector("#submit");
 const aot_reset = document.querySelector("#reset");
 const msg = document.querySelector("#output");
 
 const showMsg = (text, color, sound_selector) => {
-  const policeWhistleSE = document.querySelector("#policewhistle");
+  const infoSE = document.querySelector("#infoSE");
   const failSE = document.querySelector("#fail-sound-effect");
   const successSE = document.querySelector("#success-sound-effect");
   const resetSE = document.querySelector("#reset-sound-effect");
 
+  msg.style.display = "inline";
+  msg.style.color = color;
+  msg.innerText = text;
+
   switch (sound_selector) {
     case "success":
       successSE.play();
-      msg.style.display = "inline";
-      msg.style.color = color;
-      msg.innerText = text;
       break;
 
     case "fail":
       failSE.play();
-      msg.style.display = "inline";
-      msg.style.color = color;
-      msg.innerText = text;
       break;
 
     case "info":
-      policeWhistleSE.play();
-      msg.style.display = "inline";
-      msg.style.color = color;
-      msg.innerText = text;
+      infoSE.play();
       break;
 
     case "reset":
@@ -50,7 +45,16 @@ aot_submit.addEventListener("click", () => {
   side2 = Number(s2.value);
   side3 = Number(s3.value);
 
-  const areaOfTriangle = (s1, s2, s3) => {};
+  const areaOfTriangle = (s1, s2, s3) => {
+    semi_perimeter = (s1 + s2 + s3) / 2;
+
+    return (aot = Math.sqrt(
+      semi_perimeter *
+        (semi_perimeter - s1) *
+        (semi_perimeter - s2) *
+        (semi_perimeter - s3)
+    ).toFixed(4));
+  };
 
   if (side1 && side2 && side3) {
     if (
@@ -58,19 +62,19 @@ aot_submit.addEventListener("click", () => {
       side2 + side3 > side1 &&
       side1 + side3 > side2
     ) {
-      console.log("side 1: " + side1 + "side 2: " + side2 + "side 3: " + side3);
-      areaOfTriangle(side1, side2, side3);
+      console.log(
+        "\nside 1: " + side1 + "\nside 2: " + side2 + "\nside 3: " + side3
+      );
+      const aot = areaOfTriangle(side1, side2, side3);
+      message = "Area of triangle: " + aot;
+      showMsg(message, "green", "success");
     } else {
-      message = "The input given is not a triangle";
-      color = "red";
-      ss = "fail";
-      showMsg(message, color, ss);
+      message = "This is not a triangle";
+      showMsg(message, "red", "fail");
     }
   } else {
     message = "Please enter all the angle to check the triangle";
-    color = "darkblue";
-    ss = "info";
-    showMsg(message, color, ss);
+    showMsg(message, "darkblue", "info");
   }
 });
 
